1a05d61b4e41e2ae5e70a2500b2ef55635d65c70
Bellmannn <108678804+Bellmannn@users.noreply.github.com>
2026-08-15 20:00:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const KASA = "rezerv-kutugu-v1";
+const DOSYALAR = ["./","./index.html","./manifest.json","./icon-192.png",
+                  "./icon-512.png","./apple-touch-icon.png","./favicon.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(KASA).then(c => c.addAll(DOSYALAR)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(k => Promise.all(k.filter(x => x !== KASA).map(x => caches.delete(x))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(v => v || fetch(e.request).then(y => {
+      if(y.ok && new URL(e.request.url).origin === location.origin){
+        const kopya = y.clone();
+        caches.open(KASA).then(c => c.put(e.request, kopya));
+      }
+      return y;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
