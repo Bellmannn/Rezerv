6f03c1629fa9a5d586c2aa18f89441dc70f6585b
Bellmannn <108678804+Bellmannn@users.noreply.github.com>
2026-08-16 12:59:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-const KASA = "rezerv-kutugu-v1";
+const KASA = "tki-rezerv-v2";
 const DOSYALAR = ["./","./index.html","./manifest.json","./icon-192.png",
-                  "./icon-512.png","./apple-touch-icon.png","./favicon.png"];
-
+                  "./icon-512.png","./icon-maskable.png","./apple-touch-icon.png","./favicon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(KASA).then(c => c.addAll(DOSYALAR)).then(() => self.skipWaiting()));
 });
@@ -12,13 +11,11 @@ self.addEventListener("activate", e => {
 });
 self.addEventListener("fetch", e => {
   if(e.request.method !== "GET") return;
-  e.respondWith(
-    caches.match(e.request).then(v => v || fetch(e.request).then(y => {
-      if(y.ok && new URL(e.request.url).origin === location.origin){
-        const kopya = y.clone();
-        caches.open(KASA).then(c => c.put(e.request, kopya));
-      }
-      return y;
-    }).catch(() => caches.match("./index.html")))
-  );
+  e.respondWith(caches.match(e.request).then(v => v || fetch(e.request).then(y => {
+    if(y.ok && new URL(e.request.url).origin === location.origin){
+      const kopya = y.clone();
+      caches.open(KASA).then(c => c.put(e.request, kopya));
+    }
+    return y;
+  }).catch(() => caches.match("./index.html"))));
 });
